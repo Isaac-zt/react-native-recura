afcfae14811aa4a4422789a06b7d308c7e552362
Isaac <isaacekaro864@gmail.com>
2026-07-31 17:12:59 +0100

    implemented fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,22 +8,10 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
   return (
     <SafeAreaView className="flex-1  bg-background p-5"> 
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
-      <Link href ="/Onboarding" className="mt-4 rounded bg-primary text-white p-4 ">Go to onboarding</Link>
-      <Link href ="/(auth)/sign-in" className="mt-4 rounded bg-primary text-white p-4 ">Go to sign in</Link>
-      <Link href ="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4 ">Go to sign up</Link>
-
-      <Link href = "/subscriptions/spotify">Spotify Subscription</Link>
-      <Link
-           href={{
-                 pathname: "/subscriptions/[id]",
-                  params: { id: "claude" },
-           }}
-      >
-        Claude Subscription
-      </Link>      
+      <Text className="text-5xl font-sans-extrabold"> Home </Text>
+      <Link href ="/Onboarding" className="mt-4 font-sans-bold rounded bg-primary text-white p-4 ">Go to onboarding</Link>
+      <Link href ="/(auth)/sign-in" className="mt-4 font-sans-bold rounded bg-primary text-white p-4 ">Go to sign in</Link>
+      <Link href ="/(auth)/sign-up" className="mt-4 font-sans-bold rounded bg-primary text-white p-4 ">Go to sign up</Link>
     </SafeAreaView>
   );
 }
